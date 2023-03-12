2066aa1af2532b3b2cd1cced136c8b6b35ec1573
alropidi93 <alvaro.picho@pucp.edu.pe>
2023-03-12 02:16:30 -0500

    authorization and authentication

---
+++ src/main/java/com/picho/reverseauctionblockchain/ReverseAuctionBlockhainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.picho.reverseauctionblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseAuctionBlockhainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverseAuctionBlockhainBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picho/reverseauctionblockchain/ReverseAuctionBlockhainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picho.reverseauctionblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseAuctionBlockhainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
